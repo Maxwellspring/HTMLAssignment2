12ecddf36a0376c5a013737e733552f3073a31f4
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2024-09-06 10:44:45 -0700

    es stuff
    yea

--- function.js
+++ function.js
@@ -1,7 +1,20 @@
-function Changeletext() {
+// 
 
+// function ShowStuff() {
+    // var T = document.getElementById("ShowMochi");
+    // T.style.display = "block";  // <-- Set it to block
+// }
+
+// function Changeletext() {
+
+
+
+// const text = document.getElementById("JUAN")
+
+// document.getElementById("TREE").innerText = text
+// }
+
+function TestsFunction() {
+    var T = document.getElementById("TestsDiv");
+    T.style.display = "block";  // <-- Set it to block
 }
-
-const text = document.getElementById("JUAN")
-
-document.getElementById("TREE").innerText = text
