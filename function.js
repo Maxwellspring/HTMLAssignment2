51c3a1f07a86bfec7af832304ba9e30650e5470b
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2024-09-09 10:45:15 -0700

    YEA

--- function.js
+++ function.js
@@ -14,7 +14,25 @@
 // document.getElementById("TREE").innerText = text
 // }
 
-function TestsFunction() {
-    var T = document.getElementById("TestsDiv");
-    T.style.display = "block";  // <-- Set it to block
-}
+// function TestsFunction() {
+//     var T = document.getElementById("TestsDiv");
+//     T.style.display = "block";  // <-- Set it to block
+// }
+
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('showtrash');
+    const imageContainer = document.querySelector('.trashymochi');
+
+    button.addEventListener('click', () => {
+        if (imageContainer.style.display === 'none' || imageContainer.style.display === '') {
+            imageContainer.style.display = 'show';
+            button.textContent = 'Hide Images';
+        } else {
+            imageContainer.style.display = 'none';
+            button.textContent = 'Show Images';
+        }
+    });
+});
